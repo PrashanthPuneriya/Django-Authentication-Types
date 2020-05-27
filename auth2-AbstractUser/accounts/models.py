5efe4ea451957448578a6cd203126e2a7d4a8136
Prashanth Puneriya <puneriyaprashanth@gmail.com>
2020-05-27 17:52:26 +0530

    Added max_length constraint to email field

--- auth2-AbstractUser/accounts/models.py
+++ auth2-AbstractUser/accounts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class MyUser(AbstractUser):
-    email = models.EmailField(verbose_name='email', unique=True)
+    email = models.EmailField(verbose_name='email', max_length=255, unique=True)
     
     USERNAME_FIELD = 'email'
     EMAIL_FIELD = 'email'
