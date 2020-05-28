112bcd2e9b1d0257f369c2d5ca71829c5b05299b
Prashanth Puneriya <puneriyaprashanth@gmail.com>
2020-05-28 14:39:27 +0530

    Added Comments

--- auth2-AbstractUser/accounts/models.py
+++ auth2-AbstractUser/accounts/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 class MyUser(AbstractUser):
     email = models.EmailField(verbose_name='email', max_length=255, unique=True)
     
+    # The `USERNAME_FIELD` property tells us which field we will use to log in.
     USERNAME_FIELD = 'email'
     EMAIL_FIELD = 'email'
 
