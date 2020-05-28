47d0094997cf3165f5d592c1eee6e26e2ef96484
Prashanth Puneriya <puneriyaprashanth@gmail.com>
2020-05-28 15:14:09 +0530

    Removes unrequired files

--- DRF-TokenBasedAuth/main/urls.py
+++ DRF-TokenBasedAuth/main/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('api.urls', namespace='accounts')),
-
+    
     path('api/v1/', include('api.urls', namespace='apiaccounts')),
 ]
