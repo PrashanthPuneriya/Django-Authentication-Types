c4c5b58d2f66a441dbf920da8e64fbc7191b6175
Prashanth Puneriya <puneriyaprashanth@gmail.com>
2020-07-05 23:14:39 +0530

    Restructure folders

--- ToDoList/todolist/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import ToDo
-
-class ToDoAdmin(admin.ModelAdmin):
-    list_display = ('name', 'user', 'due_date')
-
-admin.site.register(ToDo, ToDoAdmin)
-
